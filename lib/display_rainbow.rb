3160bf02958ebe1dbf418bf3d4e18e08d8495e53
Jalen Roberts <jalenmr94@hotmail.com>
2019-04-02 19:00:29 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,8 +5,8 @@ green = "G:, green"
 blue = "B:, blue"
 purple = "P:, purple"
 
-colors = ["R: red", "O: orange","Y: yellow", "G: green", "B: blue", "P: purple"]
+colors = ["R: red", "O: orange","Y: yellow", "G: green", "B: blue", "I: indigo" "V: violet"]
 
 def display_rainbow(colors)
-puts "R: #{colors[0]} O: #{colors[1]} Y: #{colors[2]} G: #{colors[3]} B: #{colors[4]} P: #{colors[5]}"
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
